8b65d0108343c7097eb3c8f470ab5fe9e2ff1fb7
Joshua Youden <Joshua.Youden@keyin.com>
2024-10-29 14:34:51 -0230

    Tests done and working!

--- __tests__/utils/restaurant.test.js
+++ __tests__/utils/restaurant.test.js
@@ -1,16 +1,32 @@
 const { Cuisines, Dishes } = require("../../utils/data");
-const { generateRandomMenuItem, generateMenu, selectRandomCuisine } = require("../../utils/restaurantUtils");
+const {
+  generateRandomMenuItem,
+  generateMenu,
+  selectRandomCuisine,
+} = require("../../utils/restaurantUtils");
 
-describe('Restaurant Functions', () => {
-    describe('generateRandomMenuItem', () => {
-      // Test implementations go here
+describe("Restaurant Functions", () => {
+  describe("generateRandomMenuItem", () => {
+    test("Test should return a random menu item from a given cuisine", () => {
+      const cuisine = Object.keys(Dishes)[0];
+      const menuItem = generateRandomMenuItem(cuisine);
+      expect(menuItem).toHaveProperty("name");
+      expect(menuItem).toHaveProperty("description");
     });
+  });
 
-    describe('generateMenu', () => {
-      // Test implementations go here
+  describe("generateMenu", () => {
+    test("Test should return between 5 and 10 menu items", () => {
+      const menu = generateMenu();
+      expect(menu.items.length).toBeGreaterThanOrEqual(5);
+      expect(menu.items.length).toBeLessThanOrEqual(10);
     });
+  });
 
-    describe('selectRandomCuisine', () => {
-      // Test implementations go here
+  describe("selectRandomCuisine", () => {
+    test("Test should return a random cuisine from the Cuisines array", () => {
+      const cuisine = selectRandomCuisine();
+      expect(Cuisines).toContain(cuisine);
     });
+  });
 });
